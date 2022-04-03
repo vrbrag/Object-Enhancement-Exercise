2169296163da30a1907ef37eeb45505ff5ded420
Vanessa Bragdon <vanessabragdon@Vanessas-MBP.home>
2022-04-03 15:52:46 -0700

    add all exercise files

---
+++ app.js
@@ -0,0 +1,51 @@
+// Same keys and values
+// function createInstructor(firstName, lastName){
+//    return {
+//      firstName: firstName,
+//      lastName: lastName
+//    }
+//  }
+/* Write an ES2015 Version */
+function createInstructor(firstName, lastName) {
+   return {
+      firstName, lastName
+   }
+}
+
+// Computed Property Names
+// var favoriteNumber = 42;
+
+// var instructor = {
+//   firstName: "Colt"
+// }
+
+// instructor[favoriteNumber] = "That is my favorite!"
+/* Write an ES2015 Version */
+let favoriteNumber = 42
+const instructor = {
+   firstName: 'Colt',
+   [favoriteNumber]: "That's my favorite number!"
+}
+
+// createAnimal function
+// Write a function which generates an animal object. The function should accepts 3 arguments:
+
+// species: the species of animal (‘cat’, ‘dog’)
+// verb: a string used to name a function (‘bark’, ‘bleet’)
+// noise: a string to be printed when above function is called (‘woof’, ‘baaa’)
+// Use one or more of the object enhancements we’ve covered.
+
+const d = createAnimal("dog", "bark", "Woooof!") // {species: "dog", bark: ƒ}
+d.bark()  //"Woooof!"
+
+const s = createAnimal("sheep", "bleet", "BAAAAaaaa") // {species: "sheep", bleet: ƒ}
+s.bleet() //"BAAAAaaaa"
+
+function createAnimal(species, verb, noise) {
+   return {
+      species,
+      [verb]() {
+         return noise
+      }
+   }
+}
